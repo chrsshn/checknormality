ac5d355809dbcc8a6cf37620e9ff7a29f736c24c
Chris Shin <chrsshn@gmail.com>
2020-11-23 15:05:14 -0600

    added comments to sw_test

--- R/sw_test.R
+++ R/sw_test.R
@@ -2,7 +2,7 @@
 #'
 #' Test the normality of a vector using a version of the Shapiro-Wilk test
 #'
-#' @param x vector
+#' @param vec_value vector
 #' @param version character
 #' @param alpha character
 #'
@@ -12,41 +12,52 @@
 #' @examples
 #' sw_test (rnorm(10, 0, 1))
 #'
-sw_test <- function (x,
+sw_test <- function (vec_value,
                      version = "original",
                      alpha = 0.05) {
 
+  #the original Shapiro-Wilk test is limited to n = 50 values
+  #need to check that x is a vector
+  n <- length (vec_value)
 
-  n = length (x)
-  # n = 6
+  #dat_value holds the values of the data
+  #column 'original' holds original data as given
+  #column 'sorted' holds data sorted in increasing value
+  dat_value <- data.frame (original = vec_value,
+                           sorted = sort (vec_value))
+
+  #dat_coef holds values used to calculate W
+  #column 'a' holds the coefficients a_i for the calculation of the W statistic
+  #the values of the coefficients comes from an internal table
+  #the number of coefficients varies with n
   dat_coef <- as.data.frame(stats::na.omit (sw_coefs[,as.character(n)]))
-  # print (nrow(dat_coef))
-  dat_coef$diff = rep (0, nrow (dat_coef))
+  names (dat_coef) <- 'a'
 
+  #column 'diff' holds the difference between the lowest and highest data values (e.g. between 1st and last, 2nd and second-to-last, etc.)
+  dat_coef$diff <- rep (0, nrow (dat_coef))
 
-  ordered_vals = sort (x)
+  for (i in 1:(n/2)) {
+    dat_coef$diff[i] <- dat_value$sorted[(n-i+1)]-dat_value$sorted[i]
 
-   for (i in 1:(n/2)) {
-     dat_coef$diff[i] = ordered_vals[(n - i + 1)] -
-       ordered_vals[i]
+  }
 
-   }
+  #column 'a_diff' holds the product of the a_i coefficient and the ith difference
+  dat_coef$a_diff <- dat_coef[,1] * dat_coef[,2]
 
+  #b is an intermediate value used to calculate W
+  b <- sum (dat_coef[,3])
 
+  #SS is an intermediate value used to calculate W
+  SS <- sum((dat_value$sorted-mean(dat_value$sorted))^2)
 
-   dat_coef$a_diff = dat_coef[,1] * dat_coef[,2]
+  #W is the test statistic
+  W <- (b^2)/SS
 
-   b = sum (dat_coef[,3])
+  p_02 <- unlist(sw_pvals[n,2])
+  p_05 <- unlist (sw_pvals[n,3])
 
-   SS =  sum( (ordered_vals - mean(ordered_vals) )^2 )
-
-   W = (b^2)/SS
-
-   p_02 <- unlist(sw_pvals[n,2])
-   p_05 <- unlist (sw_pvals[n,3])
-
-   p_val <- .05 - ((.03)*(p_05 - W)/(p_05 - p_02))
-   reject_decision <- ifelse (p_val <= alpha, "yes", "no")
+  p_val <- .05 - ((.03)*(p_05 - W)/(p_05 - p_02))
+  reject_decision <- ifelse (p_val <= alpha, "yes", "no")
 
   # print ("Shapiro-Wilk Test of Normality")
   # print (paste0("W = ", W, ", p = ", p_val))
@@ -56,4 +67,4 @@ sw_test <- function (x,
 
   return (round (W, 5))
 
-  }
+}
--- tests/testthat/test-sw_test.R
+++ tests/testthat/test-sw_test.R
@@ -1,31 +1,4 @@
 test_that("sw_test works", {
-  # set.seed(151)
-  # nrep = 10
-  # b0 = 5
-  # b1 = -2
-  # sigma = 2
-  #
-  #
-  # ngroup = 2
-  # group = rep( c("group1", "group2"), each = nrep)
-  # eps = rnorm(n = ngroup*nrep, mean = 0, sd = sigma)
-  # response = b0 + b1*(group == "group2") + eps
-  # sim_dat = data.frame(group, response)
-  # sim_dat
-  #
-  #
-  # lin_reg <- lm (data = sim_dat, formula = response ~ group)
-  # sum_lin <- summary (lin_reg)
-  #
-  # sum_lin$residuals
-  #
-  # sim_dat <- sim_dat %>%
-  #   add_column (fitted_vals = lin_reg$fitted.values) %>%
-  #   mutate (lm_residuals = response - fitted_vals)
-  #
-  # shapiro.test(sum_lin$residuals)
-  # checktheresiduals::sw_test(sum_lin$residuals)
-
 
 
   expect_equivalent (shapiro.test(1:5)$statistic, sw_test(1:5), tolerance = 0.002)
