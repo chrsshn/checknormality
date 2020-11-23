1bee7086b5aaa774794aaf5f2776024dac4c14dd
Chris Shin <chrsshn@gmail.com>
2020-11-22 20:12:54 -0600

    remove hello world

---
+++ R/sw_test.R
@@ -0,0 +1,16 @@
+#' Shapiro-Wilk Test
+#'
+#' Test the normality of a vector using the Shapiro-Wilk test
+#'
+#' @param x vector
+#'
+#' @return p_value numeric
+#' @export
+#'
+#' @examples
+#' sw_test (rnorm(100, 0, 1))
+#'
+sw_test <- function (x) {
+  print ("works")
+
+  }
